bc62c1a70a40ceea2dc69500afd3e22d770bc9c1
PujariVandana <pujarivandu@gmail.com>
2025-10-16 10:57:55 +0530

    16 Adding task into localhost

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Taskform from "./Compenets/Taskform";
-import Tasklist from "./Compenets/Tasklist";
-import Progresstracker from "./Compenets/ProgressTracker";
+import Taskform from "./Components/Taskform";
+import Tasklist from "./Components/Tasklist";
+import Progresstracker from "./Components/ProgressTracker";
+import { useEffect, useState } from "react";
 
 function App() {
+  const[tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  })
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
   return (
     <div>
       <h1>Task Focus</h1>
       <p>Our friendly task manager</p>
-      <Taskform />
+      <Taskform addTask = {addTask}/>
       <Tasklist />
       <Progresstracker />
       <button>Clear all task</button>
