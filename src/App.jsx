4481e7f5e7d7659b21021c46aba47f6e9570b11c
PujariVandana <pujarivandu@gmail.com>
2025-10-17 11:33:29 +0530

    Task list 17

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,42 @@ import Taskform from "./Components/Taskform";
 import Tasklist from "./Components/Tasklist";
 import Progresstracker from "./Components/ProgressTracker";
 import { useEffect, useState } from "react";
+import "./Style.css";
 
-function App() {
+
+export default function App() {
   const[tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   })
+
   const addTask = (task) => {
     setTasks([...tasks,task]);
   }
+
+  const updateTask = (updatedTask, index) => {
+    const newTasks = [...tasks];
+    newTasks[index] = updatedTask;
+    setTasks(newTasks);
+
+  }
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index));
+  }
+
   return (
     <div>
       <h1>Task Focus</h1>
       <p>Our friendly task manager</p>
       <Taskform addTask = {addTask}/>
-      <Tasklist />
+      <Tasklist tasks = {tasks} 
+      updateTask = {updateTask} 
+      deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>Clear all task</button>
     </div>
   )
 }
 
-export default App;
